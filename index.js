f6d57409597a529f1b4ac9c4895513b388934e4f
Brandon Temba <brandontemba560@gmail.com>
2022-06-12 16:39:13 +0300

    Done.

--- index.js
+++ index.js
@@ -1,2 +1,18 @@
 
 // Write your code here
+
+const num1 = 2;
+
+const num2 = 31;
+
+const multiply = num1 * num2;
+
+const random = Math.floor( Math.random() * 1000 ) + 1;
+
+const num3 = 28;
+
+const num4 = 8;
+
+const mod = num3 % num4;
+
+const max = Math.max( 13, 15, 17, 20 );
